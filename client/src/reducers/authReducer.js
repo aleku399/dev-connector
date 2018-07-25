e03a040cf849a7fca5d8ca4080201ff1e596eced
allan <epicallan.al@gmail.com>
2018-07-25 09:15:34 +0300

    including redux

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+// import  {GET_ERRORS} from '../actions/types';
+
+// const initialState = {
+//     isAuthenticated: false,
+//     user: {}
+// };
+
+// export default (state = initialState, action) => {
+//     switch(action.type) {
+//         case TEST_DISPA:
+//         return {
+//             ...state,
+//             user: action.payload
+//         };
+//         default:
+//             return state;
+//     }
+// }
