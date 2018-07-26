b8714e927f798f89fe589bdad5fb152c45c5fd09
aleku399 <aleku399@gmail.com>
2018-07-26 13:50:03 +0300

    w.i.p

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,30 +1,55 @@
 import axios from 'axios';
-import {GET_ERRORS} from './types';
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
     axios
-    .post('/api/users/register', userData)
-    .then(res => history.push('/login'))
-    .catch(err =>
-    dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-        })
-    );
+        .post('/api/users/register', userData)
+        .then(res => history.push('/login'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
 };
 
 // Login - Get User Token
 export const loginUser = userData => dispatch => {
     axios
-    .post('/api/users/login', userData)
-    .then(res => {
-        
-    })
-    .catch(err =>
-    dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
+        .post('/api/users/login', userData)
+        .then(res => {
+            // save to localStorage
+            const {
+                token
+            } = res.data;
+            // set token to 1s
+            localStorage.setItem('jwtToken', token);
+            // set token to Auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token)
+            // set current user
+            dispatch(setCurrentUser(decoded));
         })
-    );
-}
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// Log user out
+export const logoutUser = userData => dispatch => {
+   // Remove token from localStorage
+   localStorage.
+};
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
+};
